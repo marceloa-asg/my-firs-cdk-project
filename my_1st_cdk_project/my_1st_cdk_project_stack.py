20921caa9c0873ee80220ef6ea7e54b06f38f5f7
Marcelo Aguisky <marcelo.aguisky@txwormhole.com>
2020-11-30 14:24:23 -0500

    end of section 2

--- my_1st_cdk_project/my_1st_cdk_project_stack.py
+++ my_1st_cdk_project/my_1st_cdk_project_stack.py
@@ -1,5 +1,6 @@
 from aws_cdk import (
     aws_s3 as _s3,
+    aws_iam as _iam,
     core
     )
 
@@ -13,6 +14,32 @@ class My1StCdkProjectStack(core.Stack):
             self,
             "myBucketId",
             bucket_name="myfistcdkprojectmaguisky010",
-            versioned=True,
-            encryption=_s3.BucketEncryption.KMS_MANAGED
+            versioned=False,
+            encryption=_s3.BucketEncryption.S3_MANAGED,
+            block_public_access=_s3.BlockPublicAccess.BLOCK_ALL
+        )
+        mybucket = _s3.Bucket(
+            self,
+            "myBucketId1",
+            # bucket_name="myfistcdkprojectmaguisky010",
+            # versioned=False,
+            # encryption=_s3.BucketEncryption.S3_MANAGED,
+            # block_public_access=_s3.BlockPublicAccess.BLOCK_ALL
+        )
+        snstopicname = "asbefg1234"
+        if not core.Token.is_unresolved(snstopicname) and len(snstopicname) > 10:
+            raise ValueError("Maximum value can be up to 10 characters" )
+        
+        print(mybucket.bucket_name)
+        
+        _iam.Group(self,
+                   "gid")
+        
+        output_1 = core.CfnOutput(
+            self,
+            "myBucketOutput1",
+            value=mybucket.bucket_name,
+            description=f"My first CDK Bucket",
+            export_name="myBucket1"
+            
         )
