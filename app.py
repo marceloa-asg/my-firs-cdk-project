10f5f0186a670ce020f99dc37475bd5d3f57be13
Marcelo Aguisky <marcelo.aguisky@txwormhole.com>
2020-12-02 14:37:38 -0500

    section - 3, lesson 14

--- app.py
+++ app.py
@@ -2,10 +2,17 @@
 
 from aws_cdk import core
 
-from my_1st_cdk_project.my_1st_cdk_project_stack import My1StCdkProjectStack
-
+from my_1st_cdk_project.my_1st_cdk_project_stack import MyArtifactBucketStack
 
 app = core.App()
-My1StCdkProjectStack(app, "my-1st-cdk-project")
+
+env_dev = core.Environment(account=app.node.try_get_context("dev")["account"], 
+                          region=app.node.try_get_context("dev")["region"])
+env_prod = core.Environment(account=app.node.try_get_context("prod")["account"], 
+                          region=app.node.try_get_context("prod")["region"])
+
+print (app.node.try_get_context("dev")["region"])
+MyArtifactBucketStack(app, "myDevStack", env=env_dev)
+MyArtifactBucketStack(app, "myProdStack", is_prod=True, env=env_prod)
 
 app.synth()
--- my_1st_cdk_project/my_1st_cdk_project_stack.py
+++ my_1st_cdk_project/my_1st_cdk_project_stack.py
@@ -1,45 +1,31 @@
 from aws_cdk import (
     aws_s3 as _s3,
     aws_iam as _iam,
+    aws_kms as _kms,
     core
     )
 
-class My1StCdkProjectStack(core.Stack):
+class MyArtifactBucketStack(core.Stack):
 
-    def __init__(self, scope: core.Construct, construct_id: str, **kwargs) -> None:
+    def __init__(self, scope: core.Construct, construct_id: str, is_prod=False, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
         # The code that defines your stack goes here
-        _s3.Bucket(
-            self,
-            "myBucketId",
-            bucket_name="myfistcdkprojectmaguisky010",
-            versioned=False,
-            encryption=_s3.BucketEncryption.S3_MANAGED,
-            block_public_access=_s3.BlockPublicAccess.BLOCK_ALL
-        )
-        mybucket = _s3.Bucket(
-            self,
-            "myBucketId1",
-            # bucket_name="myfistcdkprojectmaguisky010",
-            # versioned=False,
-            # encryption=_s3.BucketEncryption.S3_MANAGED,
-            # block_public_access=_s3.BlockPublicAccess.BLOCK_ALL
-        )
-        snstopicname = "asbefg1234"
-        if not core.Token.is_unresolved(snstopicname) and len(snstopicname) > 10:
-            raise ValueError("Maximum value can be up to 10 characters" )
-        
-        print(mybucket.bucket_name)
-        
-        _iam.Group(self,
-                   "gid")
-        
-        output_1 = core.CfnOutput(
-            self,
-            "myBucketOutput1",
-            value=mybucket.bucket_name,
-            description=f"My first CDK Bucket",
-            export_name="myBucket1"
+        # print (self.node.try_get_context("prod")["kms_arn"])
+        myKey = _kms.Key.from_key_arn(self,
+                                      "myKeyId",
+                                      self.node.try_get_context("prod")["kms_arn"])
+
+
+        if is_prod:
+            artifactBucket = _s3.Bucket(self,
+                                        "myProdArtifactBucketId",
+                                        versioned=True,
+                                        encryption=_s3.BucketEncryption.KMS,
+                                        encryption_key=myKey,
+                                        removal_policy=core.RemovalPolicy.RETAIN)
+        else:
+            artifactBucket = _s3.Bucket(self,
+                                 "myDevArtifactBucket",
+                                 removal_policy=core.RemovalPolicy.DESTROY)
             
-        )
