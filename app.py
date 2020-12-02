776c2c4b2bc52d907d765010b8ebd385fd77f7a1
Marcelo Aguisky <marcelo.aguisky@txwormhole.com>
2020-12-02 15:59:50 -0500

    lecture  15, custom VPC

--- app.py
+++ app.py
@@ -1,18 +1,10 @@
 #!/usr/bin/env python3
 
 from aws_cdk import core
-
-from my_1st_cdk_project.my_1st_cdk_project_stack import MyArtifactBucketStack
+from resource_stacks.custom_vpc import CustomVpcStack
 
 app = core.App()
 
-env_dev = core.Environment(account=app.node.try_get_context("dev")["account"], 
-                          region=app.node.try_get_context("dev")["region"])
-env_prod = core.Environment(account=app.node.try_get_context("prod")["account"], 
-                          region=app.node.try_get_context("prod")["region"])
-
-print (app.node.try_get_context("dev")["region"])
-MyArtifactBucketStack(app, "myDevStack", env=env_dev)
-MyArtifactBucketStack(app, "myProdStack", is_prod=True, env=env_prod)
+CustomVpcStack(app, "my-custom-vpc")
 
 app.synth()
