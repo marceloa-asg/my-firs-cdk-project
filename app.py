e2efc590c444c48ff15ef897824a27df5c4a25fb
Marcelo Aguisky <marcelo.aguisky@txwormhole.com>
2020-12-07 16:40:09 -0500

    Lesson 17 tagging from the top using tags.of().add()

--- app.py
+++ app.py
@@ -6,5 +6,8 @@ from resource_stacks.custom_vpc import CustomVpcStack
 app = core.App()
 
 CustomVpcStack(app, "my-custom-vpc")
-
+core.Tags.of(app).add(key="stack-team-support-email", 
+                      value=app.node.try_get_context('envs')['prod']["stack-team-support-email"])
+core.Tags.of(app).add(key="stack-level-tagging", 
+                      value=app.node.try_get_context('envs')['prod']["stack-level-tagging"])
 app.synth()
