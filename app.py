d45c2b6ece8cb5e0a0c90574fabfa6fdb029e838
Marcelo Aguisky <marcelo.aguisky@txwormhole.com>
2020-12-09 12:13:07 -0500

    fix more lint problems

--- app.py
+++ app.py
@@ -4,10 +4,12 @@ from aws_cdk import core
 from resource_stacks.custom_vpc import CustomVpcStack
 
 app = core.App()
+STACKTEAMSUPPORTEMAIL="stack-team-support-email"
+STACKLEVELTAGGING="stack-level-tagging"
 
 CustomVpcStack(app, "my-custom-vpc")
-core.Tags.of(app).add(key="stack-team-support-email",
-                      value=app.node.try_get_context('envs')['prod']["stack-team-support-email"])
-core.Tags.of(app).add(key="stack-level-tagging",
-                      value=app.node.try_get_context('envs')['prod']["stack-level-tagging"])
+core.Tags.of(app).add(key=STACKTEAMSUPPORTEMAIL,
+                      value=app.node.try_get_context('envs')['prod'][STACKTEAMSUPPORTEMAIL])
+core.Tags.of(app).add(key=STACKLEVELTAGGING,
+                      value=app.node.try_get_context('envs')['prod'][STACKLEVELTAGGING])
 app.synth()
